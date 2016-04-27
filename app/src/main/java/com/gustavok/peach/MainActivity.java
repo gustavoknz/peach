e4416a73a8aafe66dec16c55c30c784dd1404989
Gustavo Luiz Kieling <gustavo.kieling@eldorado.org.br>
2016-04-27 14:22:39 -0300

    *Toolbar removed; *Gradle version updated; *.idea folder added to .gitignore

--- app/src/main/java/com/gustavok/peach/MainActivity.java
+++ app/src/main/java/com/gustavok/peach/MainActivity.java
@@ -11,8 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-//TODO: animações pro: "Tchau, querida", "+1 coxinha"
-//TODO: animações contra: "Golpe não", "+1 mortadela"
+//TODO: animações pro: "Fora Dilma!", "Tchau, querida!", "+1 coxinha"
+//TODO: animações contra: "Golpe não!", "Vai ter luta!", "+1 mortadela"
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -36,28 +34,6 @@ public class MainActivity extends AppCompatActivity {
         if (tabLayout != null) {
             tabLayout.setupWithViewPager(mViewPager);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
